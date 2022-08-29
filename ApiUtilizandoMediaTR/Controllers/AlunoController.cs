d344d93e0f866a4acf88bef54f39f8c90506f499
daviaze <davisilcampos@hotmail.com>
2022-08-29 16:41:48 -0300

    Arrumando controller

--- ApiUtilizandoMediaTR/Controllers/AlunoController.cs
+++ ApiUtilizandoMediaTR/Controllers/AlunoController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ApiUtilizandoMediaTR.Controllers
 {
     [ApiController]
-    [Route("controller")]
+    [Route("[controller]")]
     public class AlunoController : ControllerBase
     {
         private readonly IMediator _mediator;
